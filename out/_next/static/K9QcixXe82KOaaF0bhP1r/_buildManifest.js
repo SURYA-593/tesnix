4ea43f368d2a0fb850651e73540fcb127fd0a26f
Balasurya <balasurya.g@zohocorp.com>
2026-04-18 17:49:14 +0530

    Add build

---
+++ out/_next/static/K9QcixXe82KOaaF0bhP1r/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/tesnix//_next/:path+",
+        "destination": "/tesnix/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
